a8d98894105cf4ba05d0a57425ff2140e0a0b1e2
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-31 20:26:37 -0400

    Install mongoose and connect mongod

--- index.js
+++ index.js
@@ -10,13 +10,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item ({
+  name: "somethinng"
+})
+
+const item2 = new Item ({
+  name: "something2"
+})
+
+const item3 = new Item ({
+  name: "something3"
+})
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to DB");
+  }
+});
+
 let workItems = [];
 
 app.get("/", (req, res) => {
   console.log("Here",typeof(getDay));
   const day = getDay();
-  res.render('list', { listTitle: day, itemList: items});
+  res.render('list', { listTitle: "Today", itemList: items});
 });
 
 app.post("/", (req, res) => {
